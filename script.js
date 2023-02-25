613a57f1086d7bb82a8f95be0ce94edc142c3381
rajshukla444500 <rajj444500@gmail.com>
2023-02-25 08:26:51 +0530

    It is a tic-tak-toe game webapp

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Welcome to Tic Tac Toe");
+let music = new Audio("music/music.mp3")
+let turn = new Audio("music/ting.mp3")
+let gameover = new Audio("music/gameover.mp3")
+let turn ="X"
